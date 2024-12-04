9c97e3ca7d635ab7603c9cb240a57ef7166e1111
KotlyarovSergey <zews-sv@rambler.ru>
2024-12-04 23:38:40 +0300

    v2.3 release. Change checkBoxes in AddFragment and added helloText in MainFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 4
-        versionName = "2.2"
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
